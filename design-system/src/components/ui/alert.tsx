4530207c223ea9dd05b6c5180371116778a55214
dennis-schaefer <dennis.schaefer94@gmail.com>
2026-03-29 11:34:08 +0200

    Added alert page

--- design-system/src/components/ui/alert.tsx
+++ design-system/src/components/ui/alert.tsx
@@ -11,6 +11,10 @@ const alertVariants = cva(
         default: "bg-card text-card-foreground",
         destructive:
           "bg-card text-destructive *:data-[slot=alert-description]:text-destructive/90 *:[svg]:text-current",
+        warning:
+          "bg-card text-[var(--chart-2)] *:data-[slot=alert-description]:text-[var(--chart-2)]/90 *:[svg]:text-current",
+        success:
+          "bg-card text-[var(--sidebar-primary)] *:data-[slot=alert-description]:text-[var(--sidebar-primary)]/90 *:[svg]:text-current",
       },
     },
     defaultVariants: {
