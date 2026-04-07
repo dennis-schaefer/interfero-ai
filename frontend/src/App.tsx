2602e32d6d6d278b0a3126922929843be7c6dc90
dennis-schaefer <dennis.schaefer94@gmail.com>
2026-04-07 15:22:24 +0200

    Created React frontend application

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '4rem' }}>
+        <h1>Welcome to Interfero</h1>
+      </div>
+  );
+}
+
+export default App;
